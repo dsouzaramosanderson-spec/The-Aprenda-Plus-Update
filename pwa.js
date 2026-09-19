c50cbf9978a0b17f8fa82d7ac39356eb078094b4
dsouzaramosanderson-spec <dsouzaramos.anderson@gmail.com>
2026-09-19 08:34:56 -0300

    Fix service worker registration path

--- pwa.js
+++ pwa.js
@@ -1,6 +1,6 @@
 // 1. Registra o Service Worker no navegador
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js')
+  navigator.serviceWorker.register('./sw.js')
     .then(() => console.log('Service Worker registrado com sucesso!'))
     .catch((err) => console.log('Erro ao registrar Service Worker:', err));
 }
